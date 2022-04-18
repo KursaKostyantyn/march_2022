ce443855ee9f291e4bc5fd18125ecdafce0e2bc3
KursaKostyantyn <kostya.kursa@gmail.com>
2022-04-19 00:26:19 +0300

    HomeWork22AP19

---
+++ HomeWorkJS1/task1/main.js
@@ -0,0 +1,141 @@
+/*let a = "hello"
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let j = -999
+let i = 123
+let k = 3.14
+let l = 2.7
+let m = 16
+let n = true
+let o = false*/
+/*console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(j)
+console.log(i)
+console.log(k)
+console.log(l)
+console.log(m)
+console.log(n)
+console.log(o)
+alert(a)
+alert(b)
+alert(c)
+alert(d)
+alert(e)
+alert(f)
+alert(j)
+alert(k)
+alert(l)
+alert(m)
+alert(n)
+alert(o)*/
+// document.writeln(a)
+// document.writeln(b)
+// document.writeln(c)
+// document.writeln(e)
+// document.writeln(f)
+// document.writeln(j)
+// document.writeln(k)
+// document.writeln(l)
+// document.writeln(m)
+// document.writeln(n)
+// document.writeln(o)
+//
+// let book = {
+//     name: "east",
+//     pages: 167,
+//     ganre: "fiction"
+// }
+// document.writeln(book)
+// console.log(book)
+//
+// let book2 = {
+//     name: 'some book',
+//     pages: 950,
+//     ganre: 'docu',
+//     authors: ["Ivan", "Semen", "Dmitro"]
+// }
+//
+// console.log(book2)
+// console.log(book2.ganre)
+// console.log(book2['ganre'])
+// console.log(book2.authors[1])
+//
+// let firstName = 'Kursa'
+// let middleName = 'Oleksiyovich'
+// let lastName = "Kostyantyn"
+//
+// let person = firstName + ' ' + lastName + ' ' + middleName
+// console.log(person)
+// console.log(`${firstName} ${lastName} ${middleName}`)
+//
+// let person1 = {
+//
+// }
+//
+// // let field = prompt('enter field')
+// // console.log(book2[field])
+//  a = 100
+//  b = '100'
+//  c = true
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// let book3 = {
+//     name: ' book',
+//     pages: 850,
+//     ganre: 'history',
+//     authors: ["Ivan", "Semen", "Dmitro"]
+// }
+//
+// let books = [
+//     book2,
+//     book3
+// ]
+// console.log(books[0])
+// console.log(books[1])
+
+// let fn=prompt('enter first name')
+// let mn=prompt('enter middle name')
+// let ln=prompt('enter last name')
+//
+// console.log(`${fn} ${mn} ${ln}`)
+
+/*
+let height = 23
+let wigth = 10
+
+let s = height*wigth
+console.log(s)*/
+
+/*let heightC = 10
+let dC = 4
+const pi = 3.14
+let s = pi*4
+let v = s * heightC
+console.log(v)*/
+
+/*
+let n = 3
+let m = 4
+let k = Math.sqrt((n**2+Math.pow(m,2)),1)
+console.log(k)*/
+// console.log(5 < 6)
+// console.log(5 > 6)
+// console.log(5 == 6)
+// console.log(5 === 6)
+// console.log((10 >= 10))
+// console.log(10 <= 10)
+// console.log(10 != 10)
+// console.log(10 < 10)
+// console.log(10 > 10)
+// console.log(123 !== 123)
+// console.log(123 === 123)
+
