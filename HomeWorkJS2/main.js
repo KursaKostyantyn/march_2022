c0b26ed64597c5672bf9ac38397130674d3dba0b
KursaKostyantyn <kostya.kursa@gmail.com>
2022-04-19 18:40:07 +0300

    HomeWorkJS22AP19

---
+++ HomeWorkJS2/main.js
@@ -0,0 +1,94 @@
+// let x = prompt('Enter number:')
+// console.log('x = ' + x)
+// let a = 1
+// a = 1
+// a = 0
+// a = -3
+// if (a != 0) {
+//     document.writeln('вірно')
+// } else {
+//     document.writeln('не вірно')
+// }
+
+// let time = prompt(' enter time')
+// if (time >= 0 && time <= 14) {
+//     document.write('перша чверть')
+// } else if ( time >= 15 && time <= 30) {
+//     document.write('друга чверть')
+// } else if (time >= 31 && time <= 44) {
+//     document.write('третя чверть')
+// } else if (time >= 45 && time <= 59) {
+//     document.write('четверта чверть')
+// } else {
+//     document.write('время указано не корректно')
+// }
+//
+// let time = +prompt(' enter time')
+
+// let time = 1
+// switch (time) {
+//     case (time>= 0 && time<= 14):
+//         document.write('перша чверть');
+//         break;
+//     case (time >= 15 && time <= 30):
+//         document.write('друга чверть');
+//         break;
+//     case (time >= 31 && time <= 44):
+//         document.write('третя чверть');
+//         break;
+//     case (time >= 45 && time <= 59):
+//         document.write('четверта чверть');
+//         break;
+//     default:
+//         document.write('время указано не корректно');
+// }
+
+// let day = prompt('enter day:')
+// if(day >= 1 && day <= 10) {
+//     document.writeln('перша декада')
+// } else if ( day >= 11 && day <= 20) {
+//     document.write('друга декада')
+// } else if (day >= 11 && day <= 20) {
+//     document.writeln('друга декада')
+// } else  if (day >= 21 && day <= 31) {
+//     document.writeln('третя декада')
+// } else {
+//     document.writeln(' день указан не коректно')
+// }
+// let day = prompt('enter day:')
+// switch (day){
+//     case '1':
+//         document.write('monday');
+//         break;
+//     case '2' :
+//         document.write('tuesday');
+//         break;
+//     case '3':
+//         document.writeln('wednesday');
+//         // break;
+//     case '4':
+//         document.writeln('thursday');
+//         break;
+//     case '5':
+//         document.write('friday');
+//         break;
+//     case '6':
+//         document.write('saturday');
+//         break;
+//     case '7':
+//         document.write('sunday');
+//         break;
+//     default:
+//         document.write('incorrect number of the day')
+// }
+
+// let a = prompt('enter number 1:')
+// let b = prompt('enter number 2:')
+//
+// if (a > b) {
+//     document.writeln(a)
+// } else if (a < b) {
+//     document.writeln(b)
+// } else if (a = b) {
+//     document.writeln(`number are equavalent ${a} ${'='} ${b}`)
+// }
